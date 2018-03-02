d8b6468745eb03b5e5dbf16c74307d194c195799
Pedro Pereira <pedrom314@gmail.com>
2018-03-01 23:46:52 -0300

    Bird Animation
    - Implemented the animation of "flapping wings"
    - Implement the falling bird animation

--- core/src/com/decisionmind/game/MyGdxGame.java
+++ core/src/com/decisionmind/game/MyGdxGame.java
@@ -13,9 +13,12 @@ public class MyGdxGame extends ApplicationAdapter {
     private Texture backgroung;
 
     //configuration attributes
-    private int moviment = 0;
     private int widthDevice;
     private int heightDevice;
+
+    private float variation = 0;
+    private float fallSpeed = 0;
+    private float verticalInitialPosition;
 	
 	@Override
 	public void create () {
@@ -28,6 +31,8 @@ public class MyGdxGame extends ApplicationAdapter {
         backgroung = new Texture("fundo.png");
         widthDevice = Gdx.graphics.getWidth();
         heightDevice = Gdx.graphics.getHeight();
+
+        verticalInitialPosition = heightDevice / 2;
 	}
 
 	@Override
@@ -36,11 +41,18 @@ public class MyGdxGame extends ApplicationAdapter {
         Gdx.gl.glClearColor(1,1,1,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        moviment = moviment + 1;
+        variation += Gdx.graphics.getDeltaTime()*10;
+        fallSpeed++;
+
+        if(variation > 2)
+            variation = 0;
+
+        if(verticalInitialPosition > 0)
+            verticalInitialPosition -= fallSpeed;
 
         batch.begin();
         batch.draw(backgroung,0,0, widthDevice, heightDevice);
-        batch.draw(bird[0], moviment, 300);
+        batch.draw(bird[ (int)variation ], 30, verticalInitialPosition);
         batch.end();
 	}
 
