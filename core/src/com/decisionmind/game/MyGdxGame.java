f189b47e9a9cef82912eebc2193a1a724f7cee7a
Pedro Pereira <pedrom314@gmail.com>
2018-03-01 23:38:43 -0300

    Inserting image in the application
    - Deleted the method dispose on file FlappyBird.java
    - Erase the methods render and create in the file FlappyBird.java
    - Uploaded the images files to the project structure
    - Included the first bird imagem on the app
    - Testing the functionality of method create and render

--- core/src/com/decisionmind/game/MyGdxGame.java
+++ core/src/com/decisionmind/game/MyGdxGame.java
@@ -7,27 +7,28 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+    private SpriteBatch batch;
+    private Texture[] bird;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        batch = new SpriteBatch();
+        bird = new Texture[3];
+        bird[0] = new Texture("passaro1.png");
+        bird[1] = new Texture("passaro2.png");
+        bird[2] = new Texture("passaro3.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+        Gdx.gl.glClearColor(1,1,1,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        batch.draw(bird[0], 300, 300);
+        batch.end();
 	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
 }
