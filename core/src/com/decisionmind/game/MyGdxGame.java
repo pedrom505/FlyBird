1c13941aea24b2b046167070bc118c2d118425d4
pedrom505 <pedrom314@gmail.com>
2018-03-10 17:21:47 -0300

    Score
    - Was Implement the mechanism of score. In this mechanism the player will score when the bird passes through the pipe.
    - Was change the rule to detect bird touch on the bottom side of the screen
    - Was implement the rule to detect bird touchon the top side of the screen
    - Added comments
    - Reduced the distance between top and bottom pipe
    - Removed the badge

--- core/src/com/decisionmind/game/MyGdxGame.java
+++ core/src/com/decisionmind/game/MyGdxGame.java
@@ -2,8 +2,10 @@ package com.decisionmind.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import java.util.Random;
@@ -17,21 +19,29 @@ public class MyGdxGame extends ApplicationAdapter {
     private Texture backgroung;
     private Texture higherPipe;
     private Texture lowerPipe;
-    private Texture badge;
     private Random rand;
+    private BitmapFont font;
+    private int score = 0;
+    private boolean scoreFlag = true;
 
     //configuration attributes
     private int widthDevice;
     private int heightDevice;
+    private int gameState = 0; //0 -> game is stopped 1 -> start the game
 
+    //Bird Variables
     private float variation = 0;
     private float fallSpeed = 0;
-    private float verticalInitialPosition_Bird;
-    private float horizontalPosition_Pipe;
+
+    private float verticalPosition_Bird;
+    private float horizontalPosition_Bird = 120;
+
     private float verticalPosition_Pipe;
-    private float spaceBetweenPipes = 400;
-    private float deltaTime;
+    private float horizontalPosition_Pipe;
+    private float spaceBetweenPipes = 300;
     private int verticalPositionLimit;
+
+    private float deltaTime;
 	
 	@Override
 	public void create () {
@@ -42,8 +52,12 @@ public class MyGdxGame extends ApplicationAdapter {
         bird[2] = new Texture("passaro3.png");
         higherPipe = new Texture("cano_topo_maior.png");
         lowerPipe = new Texture("cano_baixo_maior.png");
-        badge = new Texture("logo.png");
+
         rand = new Random();
+        font = new BitmapFont();
+        font.setColor(Color.WHITE);
+        font.getData().setScale(6);
+
 
         //configuration attributes
         backgroung = new Texture("fundo.png");
@@ -53,7 +67,7 @@ public class MyGdxGame extends ApplicationAdapter {
         verticalPositionLimit = (int) (lowerPipe.getHeight() + spaceBetweenPipes/2 - heightDevice/2);
         horizontalPosition_Pipe = widthDevice;
         verticalPosition_Pipe = 0;
-        verticalInitialPosition_Bird = heightDevice / 2;
+        verticalPosition_Bird = heightDevice / 2;
 
 	}
 
@@ -63,33 +77,64 @@ public class MyGdxGame extends ApplicationAdapter {
         deltaTime = Gdx.graphics.getDeltaTime();
 
         variation += deltaTime * 10;
-        horizontalPosition_Pipe -= deltaTime * 300;
-        fallSpeed++;
 
         if(variation > 2)
             variation = 0;
 
-        if(horizontalPosition_Pipe < -higherPipe.getWidth()){
-            horizontalPosition_Pipe = widthDevice;
-            verticalPosition_Pipe = rand.nextInt(verticalPositionLimit*2) - verticalPositionLimit;
+        if(gameState == 0) {
+            //Wait for the first touch of the user to start the game
+            if(Gdx.input.justTouched()){
+                gameState = 1;
+            }
+        }else{
+
+            //
+            horizontalPosition_Pipe -= deltaTime * 500;
+            fallSpeed++;
+
+            //Restart the pipe position when it cross the screen
+            if(horizontalPosition_Pipe < -higherPipe.getWidth()){
+                horizontalPosition_Pipe = widthDevice;
+                verticalPosition_Pipe = rand.nextInt(verticalPositionLimit*2) - verticalPositionLimit;
+                scoreFlag = true;
+            }
+
+            //Detects when the bird passes through the pipe
+            if(horizontalPosition_Pipe < horizontalPosition_Bird){
+                if (scoreFlag){
+                    scoreFlag = false;
+                    score++;
+                }
+            }
+
+            //Detect the touch event
+            if(Gdx.input.justTouched()){
+                gameState = 1;
+                fallSpeed = -20;
+            }
+
+
+            //if(verticalPosition_Bird > 5 || fallSpeed < 0)
+            verticalPosition_Bird -= fallSpeed;
+
+            if(verticalPosition_Bird < 0){
+                verticalPosition_Bird = 0;
+            }
+            if(verticalPosition_Bird > heightDevice - bird[0].getHeight()){
+                verticalPosition_Bird = heightDevice - bird[0].getHeight();
+                fallSpeed = 0;
+            }
+
         }
 
-        if(Gdx.input.justTouched()){
-            Gdx.app.log("Toque", "Toque na tela");
-            fallSpeed = -15;
-        }
-
-        if(verticalInitialPosition_Bird > 5 || fallSpeed < 0)
-            verticalInitialPosition_Bird -= fallSpeed;
-
-
+        // Draw all elements of the game
         batch.begin();
 
         batch.draw(backgroung, 0, 0, widthDevice, heightDevice);
-        batch.draw(badge, 0, heightDevice - badge.getHeight());
         batch.draw(higherPipe, horizontalPosition_Pipe, heightDevice / 2 + verticalPosition_Pipe + spaceBetweenPipes/2);
         batch.draw(lowerPipe, horizontalPosition_Pipe, heightDevice / 2 - spaceBetweenPipes/2 + verticalPosition_Pipe - lowerPipe.getHeight());
-        batch.draw(bird[ (int)variation ], 30, verticalInitialPosition_Bird);
+        batch.draw(bird[ (int)variation ], horizontalPosition_Bird, verticalPosition_Bird);
+        font.draw(batch, String.valueOf(score), widthDevice/2, heightDevice-heightDevice*0.02f );
 
         batch.end();
     }
